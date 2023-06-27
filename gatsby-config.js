fe884e7d1bd65a2f1cb7be553c8ad80ab3623154
Ahn Hyesu <linim@naver.com>
2023-06-28 00:14:41 +0900

    Finished Gatsby Tutorial Part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,11 @@
  */
 module.exports = {
   siteMetadata: {
+    title: "My First Gatsby Site",
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 }
